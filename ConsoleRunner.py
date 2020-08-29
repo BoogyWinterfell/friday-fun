70c7f95dc63eec5db549e68c02daeab1723b7851
Ben Gordon <19689644+BoogyWinterfell@users.noreply.github.com>
2020-08-29 19:20:43 +0300

    Added: -Some structure to ConsoleRunner.

--- ConsoleRunner.py
+++ ConsoleRunner.py
@@ -1,4 +1,22 @@
+from plistlib import Dict
+
+from Abstract.Bot import Bot
+from Abstract.GameInfo import GameInfo
+from Abstract.GameRunner import GameRunner
+from DungeonCrawl.DungeonCrawlGameEngine import DungeonCrawlGameEngine
+
+
 def main():
+    engine = DungeonCrawlGameEngine()
+    info = get_initial_game_state()
+    runner = GameRunner(engine, info)
+
+
+def register_bots() -> Dict[str, Bot]:
+    pass
+
+
+def get_initial_game_state() -> GameInfo:
     pass
 
 
