298d4f1b99343196e9958f2d2587dcc88377b2d6
Ben Gordon <19689644+BoogyWinterfell@users.noreply.github.com>
2020-09-27 10:40:17 +0300

    Modified: -Updated GameEngines' API.

--- Abstract/GameEngine.py
+++ Abstract/GameEngine.py
@@ -1,5 +1,5 @@
 import abc
-from typing import List
+from typing import List, Dict
 
 from Abstract.GameAction import GameAction
 from Abstract.EngineGameInfo import EngineGameInfo
@@ -7,5 +7,5 @@ from Abstract.EngineGameInfo import EngineGameInfo
 
 class GameEngine(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def resolve_actions(self, actions: List[List[GameAction]], game_state: EngineGameInfo):
+    def resolve_actions(self, actions: Dict[str, List[GameAction]], game_state: EngineGameInfo):
         pass
--- DungeonCrawl/DungeonCrawlGameEngine.py
+++ DungeonCrawl/DungeonCrawlGameEngine.py
@@ -1,14 +1,14 @@
-from typing import Tuple, List
+from typing import List, Dict
 
 from Abstract.ActionResolver import ActionResolver
+from Abstract.EngineGameInfo import EngineGameInfo
 from Abstract.GameAction import GameAction
 from Abstract.GameEngine import GameEngine
-from Abstract.EngineGameInfo import EngineGameInfo
 
 
 class DungeonCrawlGameEngine(GameEngine):
     def __init__(self, resolvers: List[ActionResolver]):
         self.resolvers = resolvers
 
-    def resolve_actions(self, actions: Tuple[GameAction], game_state: EngineGameInfo):
+    def resolve_actions(self, actions: Dict[str, List[GameAction]], game_state: EngineGameInfo):
         pass
