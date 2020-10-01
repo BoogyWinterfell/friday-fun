88183cc28f1c92a909ca5cb991a8bf28b745f1c5
Ben Gordon <19689644+BoogyWinterfell@users.noreply.github.com>
2020-10-01 15:28:27 +0300

    Modified: Bot is now abstract.

--- Abstract/Bot.py
+++ Abstract/Bot.py
@@ -1,3 +1,4 @@
+import abc
 from typing import List
 
 from Abstract.GameAction import GameAction
@@ -5,12 +6,13 @@ from Abstract.GameObject import GameObject
 from Abstract.PlayerGameInfo import PlayerGameInfo
 
 
-class Bot:
+class Bot(metaclass=abc.ABCMeta):
     def __init__(self, items: List[GameObject] = None):
         if items is None:
             self.items = []
         else:
             self.items = items
 
+    @abc.abstractmethod
     def play_round(self, game_info: PlayerGameInfo) -> List[GameAction]:
         pass
