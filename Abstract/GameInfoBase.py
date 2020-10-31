44150995234c91a9200ae3e13ec95afb6e2ab50d
Ben Gordon <19689644+BoogyWinterfell@users.noreply.github.com>
2020-10-31 14:02:40 +0200

    Added: game_objects property to GameInfoBase.

--- Abstract/GameInfoBase.py
+++ Abstract/GameInfoBase.py
@@ -1,8 +1,12 @@
 import abc
 from dataclasses import dataclass
+from typing import List
+
+from Abstract.GameObject import GameObject
 
 
 @dataclass
 class GameInfoBase(metaclass=abc.ABCMeta):
     max_rounds: int
     round_number: int
+    game_objects: List[GameObject]
